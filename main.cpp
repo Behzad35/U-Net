c6e63ff2ea3be032769920ceae6b78bb7a5d017d
Behzad35 <104936282+Behzad35@users.noreply.github.com>
2022-06-23 11:37:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,26 +4,27 @@ class Layer{
 	int n;
 	int num_of_features = pow(2,6+n);
 	int imgsize = 512/pow(2,n);
+	typedef double valtype;
 	// =================== Array of features ==================
-	Aofind [num_of_features/2][imgsize][imgsize];
+	valtype Aofind [num_of_features/2][imgsize][imgsize];
 	// Aof1d = (128 elements/feature channels, every elmenent = 256x256)
-	Aof1d [num_of_features][imgsize][imgsize];
-	Aof2d [num_of_features][imgsize][imgsize]; // white
-	Aofinu [num_of_features][imgsize][imgsize]; // blue
-	Aof1u [num_of_features][imgsize][imgsize];
-	Aof2u [num_of_features][imgsize][imgsize];
+	valtype Aof1d [num_of_features][imgsize][imgsize];
+	valtype Aof2d [num_of_features][imgsize][imgsize]; // white
+	valtype Aofinu [num_of_features][imgsize][imgsize]; // blue
+	valtype Aof1u [num_of_features][imgsize][imgsize];
+	valtype Aof2u [num_of_features][imgsize][imgsize];
 	// ================== Array of kernels ===================
 	// Aok1d = (128 elements/kernels, every element = 3x3x64)
 	// Aok2d = (128 elements/kernels, every element = 3x3x128)
 	// Aok1u = (128 elements/kernels, every element = 3x3x256)
 	// Aok2u = (128 elements/kernels, every element = 3x3x128)
-	// Aok_uc (empty for firstlayr) = (64 elements/kernels, every element = 2x2x256)
-	// 		[number][w][h][depth]
-	Aok1d 	[num_of_features][3][3][num_of_features*0.5];
-	Aok2d 	[num_of_features][3][3][num_of_features];
-	Aok1u 	[num_of_features][3][3][num_of_features*2];
-	Aok2u 	[num_of_features][3][3][num_of_features];
-	Aok_uc 	[num_of_features*0.5][2][2][num_of_features*2];
+	// Aok_uc (empty for firstlayr) = (64 elements/kernels, every element = 2x2x128) from current layer to upper layer
+	// 		[number][depth][w][h]
+	valtype Aok1d 	[num_of_features][num_of_features/2][3][3];
+	valtype Aok2d 	[num_of_features][num_of_features][3][3];
+	valtype Aok1u 	[num_of_features][num_of_features*2][3][3];
+	valtype Aok2u 	[num_of_features][num_of_features][3][3];
+	valtype Aok_uc 	[num_of_features/2][num_of_features][2][2];
 
 
 	// ============================
@@ -31,34 +32,44 @@ class Layer{
 	// (6 errortensor arrs per layer)
 	// and maybe more...
 	// ============================
-	(pad conv and ReLU)
-	(pad the input first, then conv, apply ReLU, write to output)
-	conv(double* ker, double* feat, bool overwrite=False){}
-	conv(Aok1d, Aofind) (depth of every kernel in Aok1d is equal to number of input channels/features) --> (overwrite Aof1d)
-	conv(Aok2u, Aof1u) ---> (overwrite Aof2u)
+	// (pad conv and ReLU)
+	// (pad the input first, then conv, apply ReLU, write to output)
+	// conv(double* ker, double* feat, bool overwrite=False){}
+	// conv(Aok1d, Aofind) (depth of every kernel in Aok1d is equal to number of input channels/features) --> (overwrite Aof1d)
+	// conv(Aok2u, Aof1u) ---> (overwrite Aof2u)
 	// =============================
-	void avgpool(double* Aofind_lower){ // Aofind_lower is in the lower layer
-		int imgsize_lower = 0.5 * imgsize;
+	void avgpool(double* Aofind_lower){ // writes to lower layer
 		for (int i=0; i<num_of_features; ++i){
-			for (int j=0; j<imgsize_lower; ++j){
-				for(int k=0; k<imgsize_lower; ++k){
-					Aofind_lower[i][j][k] = 0.25*(Aof2d[i][2*j][2*k] + Aof2d[i][2*j+1][2*k] + Aof2d[i][2*j][2*k+1] + Aof2d[i][2*j+1][2*k+1]);      
+			for (int x=0; x<imgsize; x+=2){
+				for(int y=0; y<imgsize; y+=2){
+					Aofind_lower[i][x/2][y/2] = 0.25*(Aof2d[i][x][y] + Aof2d[i][x+1][y] + Aof2d[i][x][y+1] + Aof2d[i][x+1][y+1]);      
 				}
 			}
 		}
 	}
 	// ==============================
-	void upconv(double* Aofinu_upper){
-
-
-	overwrite L1.Aofinu
+	void upconv(double* Aofinu_upper){ // writes to upper layer
+		int num_of_upconv_kernels = num_of_features/2;
+		int depth_of_upconv_kernels = num_of_features;
+		for (int i=0; i<num_of_upconv_kernels; ++i){
+			for (int j=0; j<depth_of_upconv_kernels; ++j){
+				for (int x=0; x<imgsize; ++x){
+					for (int y=0; y<imgsize; ++y){
+						Aofinu_upper[i][2*x][2*y]		= Aok_uc[i][j][0][0] * Aof2u[j][x][y];
+						Aofinu_upper[i][2*x+1][2*y]		= Aok_uc[i][j][1][0] * Aof2u[j][x][y];
+						Aofinu_upper[i][2*x][2*y+1]		= Aok_uc[i][j][0][1] * Aof2u[j][x][y];
+						Aofinu_upper[i][2*x+1][2*y+1]	= Aok_uc[i][j][1][1] * Aof2u[j][x][y];
+					}
+				}
+			}
+		}
 	}
+
 };
 
 
-
-
 int main(){
+
 
 Layer layers[4];
 for (int i=0; i<=4; ++i){
@@ -81,4 +92,8 @@ layers[i+1].upconv(layers[i].Aofinu)
 layers[i].conv(layers[i].Aok1u, layers[i].Aofinu);
 layers[i].conv(layers[i].Aok1u, layers[i].Aof2d, 1);
 layers[i].conv(layers[i].Aok2u, layers[i].Aof1u);
+}
+
+
+return 0;
 }
