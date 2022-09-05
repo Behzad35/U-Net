b6433ad3c5d05645a87f297475e2aed8088a33dc
Behzad35 <104936282+Behzad35@users.noreply.github.com>
2022-09-05 02:33:24 +0200

    Delete processImages.h

--- processImages.h
+++
@@ -1,8 +0,0 @@
-#include "CImg.h"
-#include "Layer.h"
-#include "iostream"
-#include <sstream>
-#include <cmath>
-
-void ReadImages(Layer::ArrOfVols &output, int batchNr, int batchSize);
-void displayImage(Layer::ArrOfVols &output, int b);
